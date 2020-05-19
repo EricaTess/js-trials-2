d64bf26eb8fea2186e3fbf76a1725d06a6de4731
Erica Sanchez <sanchez.ericat@gmail.com>
2020-05-19 16:34:04 -0700

    completed countwords

--- objects.js
+++ objects.js
@@ -4,6 +4,19 @@
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+  let wordCount = {}
+
+  let word = phrase.split(' ')
+
+  for (let el of word) {
+    if (wordCount[el] === undefined) {
+      wordCount[el] = 1;
+    } else {
+      wordCount[el] += 1;
+    }
+  }
+
+  return wordCount;
 }
 
 
