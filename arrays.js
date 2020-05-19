cd12edd2fcd3723d216fcc96d727743baade6316
Erica Sanchez <sanchez.ericat@gmail.com>
2020-05-19 15:55:36 -0700

    initial commit

---
+++ arrays.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+
+  for(let i = 0; i < items.length; i++) {
+    console.log(`${items[i]} ${i}`)
+  }
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
