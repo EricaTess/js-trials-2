37c860eaf9663b1e2d539740b58bf3d50e154541
Erica Sanchez <sanchez.ericat@gmail.com>
2020-05-19 17:07:38 -0700

    completed words in common

--- furtherStudy.js
+++ furtherStudy.js
@@ -3,6 +3,18 @@
 
 function wordsInCommon(words1, words2) {
   // Replace this with your code
+  
+  const result = []
+
+  let words1Set = new Set(words1);
+  let words2Set = new Set(words2);
+
+  for (let item of words1Set) {
+    if (words2Set.has(item)) {
+      result.push(item);
+    }
+  }
+  return result;
 }
 
 
